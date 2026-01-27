acbf8a30633a597d8ccf313e472844a05355b846
saishshaikh <saishshaikh2323@gmail.com>
2026-01-27 16:09:11 +0530

    HTTP-METHODS

--- 05-HTTP-Methods/index.js
+++ 05-HTTP-Methods/index.js
@@ -2,10 +2,11 @@
 
 // const app = express()
 // const port = 8000
-// app.use(express.json())
+// app.use(express.json())          // middleware  //
+
 // app.post("/Contact",(req,res) => {
 //     res.send("Contact: 9022663602")
-//     let body = req.body
+//     let body = req.body                   // req.body //
 //     console.log(body)
 // })
 
@@ -25,60 +26,80 @@
 
 
 
+// ############################  req.param #########################################
+
+// import express from "express";
+
+// const app = express()
+// const port = 8000
+
+// let users = [
+//   {
+//     "id": 1,
+//     "name": "Amit Sharma",
+//     "role": "Software Developer",
+//     "salary": 60000
+//   },
+//   {
+//     "id": 2,
+//     "name": "Neha Verma",
+//     "role": "Frontend Developer",
+//     "salary": 55000
+//   },
+//   {
+//     "id": 3,
+//     "name": "Rahul Singh",
+//     "role": "Backend Developer",
+//     "salary": 65000
+//   },
+//   {
+//     "id": 4,
+//     "name": "Priya Patel",
+//     "role": "UI/UX Designer",
+//     "salary": 50000
+//   },
+//   {
+//     "id": 5,
+//     "name": "Karan Mehta",
+//     "role": "Project Manager",
+//     "salary": 75000
+//   }
+// ]
+
+// app.get("/user", (req, res) => {
+//   res.json(users)
+// })
+
+// app.get("/user/:id", (req, res) => {
+//   let id = req.params.id
+//   let existinguser = users.find((users) => (users.id == id))
+//   if (!existinguser){
+//     return res.send("404 NOT FOUND")
+//   }
+//   res.json(existinguser)
+// })
+
+
+
+// app.listen(port, () => {
+//   console.log('START SERVER AT PORT:' + port)
+// })
+
+
+// ##########################  req query #######################
+
 
 
 import express from "express";
 
-const app = express()
-const port = 8000
+const app = express();
+const port = 8000;
 
-let users = [
-  {
-    "id": 1,
-    "name": "Amit Sharma",
-    "role": "Software Developer",
-    "salary": 60000
-  },
-  {
-    "id": 2,
-    "name": "Neha Verma",
-    "role": "Frontend Developer",
-    "salary": 55000
-  },
-  {
-    "id": 3,
-    "name": "Rahul Singh",
-    "role": "Backend Developer",
-    "salary": 65000
-  },
-  {
-    "id": 4,
-    "name": "Priya Patel",
-    "role": "UI/UX Designer",
-    "salary": 50000
-  },
-  {
-    "id": 5,
-    "name": "Karan Mehta",
-    "role": "Project Manager",
-    "salary": 75000
-  }
-]
-
-app.get("/user", (req, res) => {
-  res.json(users)
-})
-
-app.get("/user/:id", (req, res) => {
-  let id = req.params.id
-  let existinguser = users.find((users) => (users.id == id))
-  if (!existinguser){
-    return res.send("404 NOT FOUND")
-  }
-  res.json(existinguser)
-})
+app.get("/search", (req, res) => {
+  console.log(req.query)
+  res.send("HELLO");
+});
 
 app.listen(port, () => {
-  console.log('START SERVER AT PORT:' + port)
-})
-
+  console.log("SERVER STARTED on port " + port);
+});
