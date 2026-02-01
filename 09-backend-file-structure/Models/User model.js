e201f933bc99018ddc9e8bd78e05bd45fcd6b1bf
saishshaikh <saishshaikh2323@gmail.com>
2026-02-01 16:31:31 +0530

    MongoDB-Mongoose

---
+++ 09-backend-file-structure/Models/User model.js
@@ -0,0 +1,24 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true
+  },
+  age: {
+    type: Number,
+    required: true
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true
+  }
+}, { timestamps: true })
+
+export const user = mongoose.model("user", userSchema)
