92ea0742bd260fa15d52a114baf0c7cc6da58557
saishshaikh <saishshaikh2323@gmail.com>
2026-02-19 20:55:22 +0530

    AUTH-PROJECT

---
+++ 11-AUTHENTICATION-PROJECT/Frontend/src/Pages/SingUp.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+import "./SingUp.css";
+
+function SingUp() {
+  const [formData, setFormData] = useState({
+    name: "",
+    email: "",
+    password: ""
+  });
+
+  const handleChange = (e) => {
+    setFormData({
+      ...formData,
+      [e.target.name]: e.target.value
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    alert("Account Created Successfully 🚀");
+  };
+
+  return (
+    <div className="signup-container">
+      <div className="signup-box">
+        <h2>Sign Up</h2>
+
+        <form onSubmit={handleSubmit}>
+          <div className="input-group">
+            <input
+              type="text"
+              name="name"
+              value={formData.name}
+              onChange={handleChange}
+              required
+            />
+            <label>Full Name</label>
+          </div>
+
+          <div className="input-group">
+            <input
+              type="email"
+              name="email"
+              value={formData.email}
+              onChange={handleChange}
+              required
+            />
+            <label>Email</label>
+          </div>
+
+          <div className="input-group">
+            <input
+              type="password"
+              name="password"
+              value={formData.password}
+              onChange={handleChange}
+              required
+            />
+            <label>Password</label>
+          </div>
+
+          <button type="submit">Create Account</button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+export default SingUp;
